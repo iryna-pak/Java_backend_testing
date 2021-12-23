abb6cd71d1272c2f03822c76a1b861b577d0df79
iryna-pak <iryna.pak@gmail.com>
2021-12-23 18:29:12 +0100

    Lesson 7 Jenkins

---
+++ target/classes/com/geekbrains/backend/test/Hello.java
@@ -0,0 +1,15 @@
+package com.geekbrains.backend.test;
+
+public class Hello {
+
+    public int summ(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    void foo () {
+
+    }
+}
